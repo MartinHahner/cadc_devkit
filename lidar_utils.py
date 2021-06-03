dca5e10fc3fd147767b4c01dcaaa9efbcd662d2b
MartinHahner <martin.hahner@gmail.com>
2021-06-03 15:49:19 +0200

    Cleanup code

--- lidar_utils.py
+++ lidar_utils.py
@@ -1,100 +1,107 @@
-#!/usr/bin/env python
-
-import numpy as np
 import cv2
 
+import numpy as np
+
+
 class lidar_utils:
-  def __init__(self, T_CAM_LIDAR):
-    self.T_CAM_LIDAR = T_CAM_LIDAR;
-    print("init lidar utils")
 
-  def project_points(self, img, lidar_path, T_IMG_CAM, T_CAM_LIDAR, dist_coeffs, DISTORTED):
-    self.T_CAM_LIDAR = T_CAM_LIDAR;
+    def __init__(self, T_CAM_LIDAR):
 
-    scan_data = np.fromfile(lidar_path, dtype=np.float32);
+        self.T_CAM_LIDAR = T_CAM_LIDAR
+        print("init lidar utils")
 
-    # 2D array where each row contains a point [x, y, z, intensity]
-    lidar = scan_data.reshape((-1, 4));
 
-    # Get height and width of the image
-    h, w = img.shape[:2]
+    def project_points(self, img, lidar_path, T_IMG_CAM, T_CAM_LIDAR, dist_coeffs, DISTORTED):
 
-    projected_points = [];
+        self.T_CAM_LIDAR = T_CAM_LIDAR
 
-    [rows, cols] = lidar.shape;
-    # print(lidar[0,:])
-    # print(lidar[1,:])
-    # print(lidar[1,0:3])
+        scan_data = np.fromfile(lidar_path, dtype=np.float32)
 
-    for i in range(rows):
-      # print(lidar[i,:])
-      p = np.array([0.0, 0.0, 0.0, 1.0]);
-      p[0:3] = lidar[i,0:3];
-      # print("p",p);
-      projected_p =  np.matmul(self.T_CAM_LIDAR, p.transpose());
-      if projected_p[2] < 2: # arbitrary cut off
-        continue;
-      projected_points.append([projected_p[0], projected_p[1], projected_p[2]]);
+        # 2D array where each row contains a point [x, y, z, intensity]
+        lidar = scan_data.reshape((-1, 4))
 
-    #print("projected_points", projected_points)
+        # Get height and width of the image
+        h, w = img.shape[:2]
 
-    # Send [x, y, z] and Transform
-    projected_points_np = np.array(projected_points)
-    image_points = self.project(projected_points_np, T_IMG_CAM, dist_coeffs, DISTORTED);
-    # print("image_points")
-    # print(image_points)
+        projected_points = []
 
-    radius = 0
+        [rows, cols] = lidar.shape
+        # print(lidar[0,:])
+        # print(lidar[1,:])
+        # print(lidar[1,0:3])
 
-    [rows, cols] = projected_points_np.shape;
+        for i in range(rows):
+            # print(lidar[i,:])
+            p = np.array([0.0, 0.0, 0.0, 1.0])
+            p[0:3] = lidar[i,0:3]
+            # print("p",p)
+            projected_p =  np.matmul(self.T_CAM_LIDAR, p.transpose())
+            if projected_p[2] < 2: # arbitrary cut off
+                continue
+            projected_points.append([projected_p[0], projected_p[1], projected_p[2]])
 
-    NUM_COLOURS = 7;
-    rainbow = [
-      [0, 0, 255], # Red
-      [0, 127, 255], # Orange
-      [0, 255, 255], # Yellow
-      [0, 255, 0], # Green
-      [255, 0, 0], # Blue
-      [130, 0, 75], # Indigo
-      [211, 0, 148] # Violet
-    ];
+        #print("projected_points", projected_points)
 
-    for i in range(rows):
-      colour = int(NUM_COLOURS*(projected_points_np[i][2]/70));
-      x = int(image_points[i][0])
-      y = int(image_points[i][1])
-      if x < 0 or x > w - 1 or y < 0 or y > h - 1:
-        continue;
-      if colour > NUM_COLOURS-1:
-        continue;
+        # Send [x, y, z] and Transform
+        projected_points_np = np.array(projected_points)
+        image_points = self.project(projected_points_np, T_IMG_CAM, dist_coeffs, DISTORTED)
+        # print("image_points")
+        # print(image_points)
 
-      cv2.circle(img, (x,y), radius, rainbow[colour], thickness=2, lineType=8, shift=0);
+        radius = 0
 
-    return img;
+        [rows, cols] = projected_points_np.shape
 
-  def project(self, p_in, T_IMG_CAM, dist_coeffs, DISTORTED):
-    p_out = []
-    [rows, cols] = p_in.shape;
+        NUM_COLOURS = 7
+        rainbow = [
+            [0, 0, 255], # Red
+            [0, 127, 255], # Orange
+            [0, 255, 255], # Yellow
+            [0, 255, 0], # Green
+            [255, 0, 0], # Blue
+            [130, 0, 75], # Indigo
+            [211, 0, 148] # Violet
+        ]
 
-    for i in range(rows):
-      # print("p_in[i]", p_in[i])
-      point = np.array([0.0, 0.0, 0.0, 1.0]);
-      # print("p_in[i][0]",p_in[i][0])
-      point[0:3] = p_in[i];
-      # print("p[0]",p[0])
-      if DISTORTED:
-        rvec = tvec = np.zeros(3)
-        # print(p[0:3])
-        # print(T_IMG_CAM[0:3,0:3])
-        # print(np.array(calib['CAM02']['distortion_coefficients']['data']))
-        image_points, jac = cv2.projectPoints(np.array([point[0:3]]), rvec, tvec, T_IMG_CAM[0:3,0:3], dist_coeffs)
-        p_out.append([image_points[0,0,0], image_points[0,0,1]]);
-        # print("image_points", image_points[0,0])
-      else:
-        curr = np.matmul(T_IMG_CAM, point.transpose()).transpose();
-        # print("curr",curr);
-        done = [curr[0] / curr[2], curr[1] / curr[2]]
-        p_out.append(done);
-        # print("p_out append", done)
+        for i in range(rows):
 
-    return p_out;
+            colour = int(NUM_COLOURS*(projected_points_np[i][2]/70))
+            x = int(image_points[i][0])
+            y = int(image_points[i][1])
+            if x < 0 or x > w - 1 or y < 0 or y > h - 1:
+                continue
+            if colour > NUM_COLOURS-1:
+                continue
+
+            cv2.circle(img, (x,y), radius, rainbow[colour], thickness=2, lineType=8, shift=0)
+
+        return img
+
+
+    def project(self, p_in, T_IMG_CAM, dist_coeffs, DISTORTED):
+
+        p_out = []
+        [rows, cols] = p_in.shape
+
+        for i in range(rows):
+            # print("p_in[i]", p_in[i])
+            point = np.array([0.0, 0.0, 0.0, 1.0])
+            # print("p_in[i][0]",p_in[i][0])
+            point[0:3] = p_in[i]
+            # print("p[0]",p[0])
+            if DISTORTED:
+                rvec = tvec = np.zeros(3)
+                # print(p[0:3])
+                # print(T_IMG_CAM[0:3,0:3])
+                # print(np.array(calib['CAM02']['distortion_coefficients']['data']))
+                image_points, jac = cv2.projectPoints(np.array([point[0:3]]), rvec, tvec, T_IMG_CAM[0:3,0:3], dist_coeffs)
+                p_out.append([image_points[0,0,0], image_points[0,0,1]])
+                # print("image_points", image_points[0,0])
+            else:
+                curr = np.matmul(T_IMG_CAM, point.transpose()).transpose()
+                # print("curr",curr)
+                done = [curr[0] / curr[2], curr[1] / curr[2]]
+                p_out.append(done)
+                # print("p_out append", done)
+
+        return p_out
--- run_demo_2d_tracklets.py
+++ run_demo_2d_tracklets.py
@@ -1,62 +1,98 @@
-#!/usr/bin/env python
-
-import json
-import numpy as np
 import cv2
-import load_calibration
-from scipy.spatial.transform import Rotation as R
+import json
+import socket
 
-frame = 0
-cam = '0'
-seq = '0069'
-DISTORTED = False
+import numpy as np
+
+from typing import Tuple
+from pathlib import Path
+from matplotlib import pyplot as plt
+from load_calibration import  load_calibration
+
+
 MOVE_FORWARD = True
-base_path = "/media/matthew/WAVELAB_2TB/winter"
-calib_path = "/media/matthew/WAVELAB_2TB/winter/calib/";
 
-if DISTORTED:
-  path_type = 'raw'
-else:
-  path_type = 'processed'
 
-img_path = base_path + "/data/" + seq + "/" + path_type + "/image_0" + cam + "/data/" + format(frame, '010') + ".png";
-annotations_file = base_path + "/data/" + seq + "/" + '/2d_annotations.json';
+def vis_gt_on_cam_2d(date: str, sequence: str, camera: str, frame: int, base_dir: str = None) -> Tuple[np.ndarray, str]:
 
-# load calibration dictionary
-calib = load_calibration.load_calibration(calib_path);
+    if base_dir:
 
-# Projection matrix from camera to image frame
-T_IMG_CAM = np.eye(4);
-T_IMG_CAM[0:3,0:3] = np.array(calib['CAM0' + cam]['camera_matrix']['data']).reshape(-1, 3);
-T_IMG_CAM = T_IMG_CAM[0:3,0:4]; # remove last row
+        BASE = Path(base_dir)
 
-dist_coeffs = np.array(calib['CAM0' + cam]['distortion_coefficients']['data'])
-
-# Load 2d annotations
-annotations_data = None
-with open(annotations_file) as f:
-    annotations_data = json.load(f)
-
-img = cv2.imread(img_path)
-img_h, img_w = img.shape[:2]
-
-# Add each box to image
-for camera_response in annotations_data[frame]['camera_responses']:
-  if camera_response['camera_used'] != int(cam):
-    continue;
-
-  for annotation in camera_response['annotations']:
-    left = int(annotation['left'])
-    top = int(annotation['top'])
-    width = int(annotation['width'])
-    height = int(annotation['height'])
-
-    if DISTORTED:
-      cv2.rectangle(img,(left,top),(left + width,top + height),(0,255,0),thickness=3)
     else:
-      pts_uv = np.array([[[left,top]],[[left + width,top + height]]], dtype=np.float32)
-      new_pts = cv2.undistortPoints(pts_uv, T_IMG_CAM[0:3,0:3], dist_coeffs, P=T_IMG_CAM[0:3,0:3])
-      cv2.rectangle(img,(new_pts[0][0][0],new_pts[0][0][1]),(new_pts[1][0][0],new_pts[1][0][1]),(0,255,0),thickness=3)
 
-cv2.imshow('image',img)
-cv2.waitKey(10000)
+        hostname = socket.gethostname()
+
+        if hostname == 'beast':
+
+            BASE = Path(f'/scratch_net/beast_second/mhahner/datasets/CADCD')
+
+        else:
+
+            BASE = Path(f'/srv/beegfs02/scratch/tracezuerich/data/datasets/CADCD')
+
+    image_path = str(BASE / date / sequence / "labeled" / f"image_0{camera}" / "data" / f"{format(frame, '010')}.png")
+    calib_path = str(BASE / date / "calib")
+
+    distorted = "raw" in image_path
+
+    annotations_file = BASE / date / sequence / "2d_annotations.json"
+
+    # load calibration dictionary
+    calib = load_calibration(calib_path)
+
+    # Projection matrix from camera to image frame
+    T_IMG_CAM = np.eye(4)
+    T_IMG_CAM[0:3,0:3] = np.array(calib['CAM0' + camera]['camera_matrix']['data']).reshape(-1, 3)
+    T_IMG_CAM = T_IMG_CAM[0:3,0:4] # remove last row
+
+    dist_coeffs = np.array(calib['CAM0' + camera]['distortion_coefficients']['data'])
+
+    # Load 2d annotations
+    with open(annotations_file) as f:
+        annotations_data = json.load(f)
+
+    img = cv2.imread(image_path)
+
+    # Add each box to image
+    for camera_response in annotations_data[frame]['camera_responses']:
+
+        if camera_response['camera_used'] != int(camera):
+            continue
+
+        for annotation in camera_response['annotations']:
+
+            left = int(annotation['left'])
+            top = int(annotation['top'])
+            width = int(annotation['width'])
+            height = int(annotation['height'])
+
+            if distorted:
+
+                cv2.rectangle(img, (left,top), (left + width,top + height), (0, 255, 0), thickness=3)
+
+            else:
+
+                pts_uv = np.array([[[left,top]],[[left + width,top + height]]], dtype=np.float32)
+                new_pts = cv2.undistortPoints(pts_uv, T_IMG_CAM[0:3,0:3], dist_coeffs, P=T_IMG_CAM[0:3,0:3])
+
+                cv2.rectangle(img,
+                              (new_pts[0][0][0], new_pts[0][0][1]),
+                              (new_pts[1][0][0], new_pts[1][0][1]), (0, 255, 0),
+                              thickness=3)
+
+    relative_path = '/'.join(image_path.split('/')[-6:])
+
+    return img, relative_path
+
+
+if __name__ == '__main__':
+
+    for cam_id in range(8):
+
+        image, title = vis_gt_on_cam_2d(date='2019_02_27', sequence='0010', camera=str(cam_id), frame=26)
+
+        plt.title(title)
+        plt.imshow(image)
+        plt.xticks([]), plt.yticks([])  # to hide tick values on x and y axis
+        plt.show()
